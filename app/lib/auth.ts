691b88996a3ac58960e063575e6e975fc7c6c548
Romario Fitzgerald <romario@Romarios-Air.routerlogin.net>
2025-10-18 23:30:38 -0500

    updated lib modules with better-auth support

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,74 +1,63 @@
+import { auth } from "~/auth.server";
 import { redisClient } from "../redis.server";
-
+import prisma from "~/prisma.server";
 interface MagicLinkResponse {
     status: 'success' | 'error';
     message?: string;
 }
 
-interface MagicLinkEmailResponse {
-    state: 'ok' | 'error';
-    data: Record<string, string>;
+export const sendMagicLink = async (email: string, request: Request): Promise<MagicLinkResponse> => {
+
+    const responseStatus = await auth.api.signInMagicLink({
+            body: {
+                email, // required
+            },
+            // This endpoint requires session cookies.
+            headers: request.headers,
+        }).then(() => 'success').catch((error)=> {
+            console.log('ERROR', error);
+            return 'error'
+        });
+    
+    return {
+        status: responseStatus,
+        message: responseStatus === 'success' ? 'Link sent successfully.' : 'Failed to send magic link email.',
+    }
 }
 
-export const sendMagicLink = async (email: string): Promise<MagicLinkResponse> => {
-    const code = generateOTP(6)
+export const verifyMagicLink = async (token: string, request: Request) => {
 
-    const currentCode = await redisClient.get(`magic_link:${email}`);
-
-    if (currentCode) {
-        return {
-            status: 'error',
-            message: 'A magic link has already been sent to this email. Please check your inbox.',
-        }
-    }
-
-    const response: MagicLinkEmailResponse = await fetch(`${process.env.MAIL_SEND_URL}`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${process.env.MAIL_SEND_API_KEY}`,
-        },
-        body: JSON.stringify({
-            "body": `Click here to login to your account: https://intellibus.hackathon.webapps.host/login/${email}/${code}`,
-            "bypass_dupe_log": true,
-            "from_email": "papertrail@fleksite.com",
-            "from_name": "PaperTrail",
-            "mail_from": "ms1.fleeksite.com",
-            "reply_to": "papertrail@fleeksite.com",
-            "subject": "Your One Time Passcode",
-            "to_email": email
-        }),
-    }).then(res => res.json()).catch(err => {
-        console.error('Error sending magic link email:', err);
-        return { state: 'error', data: { message: 'Failed to send magic link email.' } };
+    const { headers, response: verificationResponse } = await auth.api.magicLinkVerify({
+        query: { token }, // callbackURL optional if you redirect yourself
+        returnHeaders: true,
+        // if you want BA to read IP/UA for logs/rules:
+        headers: request.headers,
     });
 
-    
-    if (response.state === 'ok') {
-        await redisClient.set(`magic_link:${email}`, code, "EX", 60 * 5); // Store code in Redis with 5 min expiry
-    }
-
-    return {
-        status: response?.state  == 'ok' ? 'success' : 'error',
-        message: response?.data?.message,
-    }
-}
-
-export const verifyMagicLink = async (email: string, code: string) => {
-    const storedCode = await redisClient.get(`magic_link:${email}`);
-    if (!storedCode) {
+    if(verificationResponse.user.emailVerified) {
         return {
             status: 'error',
             message: 'Invalid or expired magic link.',
+            headers,
+            user: verificationResponse.user,
         };
-    }
-    
-    if (storedCode === code) {
-        await redisClient.del(`magic_link:${email}`); // Invalidate the code after successful verification
+    } else {
+        redisClient.get(`magiclink:email:${verificationResponse.user?.email}`).then(async (data) => {
+            if (data) {
+                const { first_name, last_name } = JSON.parse(data);
+                await prisma.user.update({
+                    where: { id: verificationResponse.user?.id },
+                    data: {
+                        first_name,
+                        last_name,
+                    },
+                });
+                redisClient.del(`magiclink:email:${verificationResponse.user?.email}`);
+            }
+        });
+
         return { status: 'success' };
     }
-
-    return  { status: 'error', message: 'Invalid magic link.' };
 }
 
 export function generateOTP(digits: number): string {
