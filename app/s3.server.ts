15bfe8d91ad81b935a296e1b5ce3fc74fb89def8
Romario Fitzgerald <romario@Romarios-MacBook-Air.local>
2025-10-19 00:51:26 -0500

    added functionality for uploads support

--- app/s3.server.ts
+++ app/s3.server.ts
@@ -1,5 +1,6 @@
-// app/utils/s3.server.ts
 import { S3Client } from "@aws-sdk/client-s3";
+import { GetObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 export const s3 = new S3Client({
   region: process.env.AWS_REGION,
@@ -8,3 +9,8 @@ export const s3 = new S3Client({
     secretAccessKey: `${process.env.AWS_SECRET_ACCESS_KEY}`,
   },
 });
+
+export async function presign(key: string, ttlSeconds = 60) {
+  const cmd = new GetObjectCommand({ Bucket: process.env.S3_BUCKET, Key: key });
+  return getSignedUrl(s3, cmd, { expiresIn: ttlSeconds });
+}
